86943174a5b972d9ddde4d26997814bc49aa60c0
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-29 19:14:22 -0300

    crud de pet

--- infraestrutura/crud/pet.js
+++ infraestrutura/crud/pet.js
@@ -3,36 +3,35 @@ const executaQuery = require('../database/queries')
 class Pet {
   lista() {
     const sql = 'SELECT * FROM Pets'
-
-    executaQuery(sql)
+    return executaQuery(sql)
   }
 
   buscaPorId(id) {
     const sql = `SELECT * FROM Pets WHERE id=${parseInt(id)}`
-
-    executaQuery(sql)
+    return executaQuery(sql).then((pets) => pets[0])
   }
 
   adiciona(item) {
     const { nome, dono, tipo, observacoes } = item
-
     const sql = `INSERT INTO Pets(nome, donoId, tipo, observacoes) VALUES('${nome}', ${dono}, '${tipo}', '${observacoes}')`
-
-    executaQuery(sql)
+    return executaQuery(sql).then(resposta => ({
+      id: resposta.insertId,
+      nome,
+      dono,
+      tipo,
+      observacoes
+    }))
   }
 
   atualiza(novoItem, id) {
     const { nome, dono, tipo, observacoes } = novoItem
-
     const sql = `UPDATE Pets SET nome='${nome}', donoId=${dono}, tipo='${tipo}', observacoes='${observacoes}' WHERE id=${id}`
-
-    executaQuery(sql)
+    return executaQuery(sql)
   }
 
   deleta(id) {
     const sql = `DELETE FROM Pets WHERE id=${id}`
-
-    executaQuery(sql)
+    return executaQuery(sql)
   }
 }
 
